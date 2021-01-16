bacfba2e85a5dc1983cbc84799a170e68f364e82
Aman Bhardwaj <inblack>
2021-01-16 11:07:23 +0530

    okay

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+const index = () => {
+  console.log( 'ok' );
+  return (
+    <div>
+      <h1>ok</h1>
+    </div>
+  );
+};
+
+export default index;
---
+++ src/utils.ts
@@ -0,0 +1,4 @@
+export const isServer = typeof window === 'undefined';
+export const isProd = process.env.NODE_ENV === 'production';
+
+export default { isProd, isServer };
